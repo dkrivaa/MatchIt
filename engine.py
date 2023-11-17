67ec2e336a4875a3fc271c7d3503a1132bcf1bb4
dkrivaa <dannykrivaa@hotmail.com>
2023-11-17 12:15:48 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -58,13 +58,12 @@ def step1():
 
 def step2():
     # Step 2
-    st.write('### Step 2 - Upload Excel file (after filling in data)')
+    st.write('### Step 2 - Upload Your Excel file (after filling in data)')
 
-    file = st.file_uploader('Choose Excel file', type=['xlsx', 'xls'])
+    file = st.file_uploader('Choose Your Excel file', type=['xlsx', 'xls'])
     if file is not None:
-        df1 = pd.read_excel(file, sheet_name='Position Prefs')
-        df2 = pd.read_excel(file, sheet_name='Candidate Prefs')
-        st.write('##### Your file was uploaded successfully')
+        df1 = pd.read_excel(file, sheet_name='Position Prefs').fillna(999)
+        df2 = pd.read_excel(file, sheet_name='Candidate Prefs').fillna(999)
 
         st.write(df1)
 
@@ -74,19 +73,64 @@ def step2():
 
         # Making values into integers:
         # relevant position by index
-        df1['position'] = df1.index + 1
+        df1.iloc[:, 0] = df1.index + 1
         # preferences by extracting number from string (if string)
         for column in df1.columns[1:]:
             df1[column] = df1[column].apply(extract_numbers_from_string, df=df2_original)
 
         # Making values into integers:
         # relevant position by index
-        df2['candidate'] = df2.index + 1
+        df2.iloc[:, 0] = df2.index + 1
         # preferences by extracting number from string (if string)
         for column in df2.columns[1:]:
             df2[column] = df2[column].apply(extract_numbers_from_string, df=df1_original)
 
         st.write(df1)
+
+        # Checking integrity of data in file
+        # Position data:
+        # checking the lack of identical preference entries
+        test1 = (df1.iloc[:, 1:].apply(lambda row: row.dropna().nunique() == len(row), axis=1))
+        st.write(test1)
+        if all(test1):
+            pass
+        else:
+            row_index = [i for i in range(0, len(test1)) if test1[i] == False]
+            st.error(f'Error: Identical preference entries in position: '
+                     f'{df1_original.iloc[row_index[0]][0]}. Please correct the error.')
+            return
+
+        # checking that all data is ok (first condition = only numbers, second condition = valid preferences)
+        if ((df1.applymap(lambda x: isinstance(x, int)).all().all()) and
+                (df1.applymap(lambda y: (y < len(df2) + 1) or (y == 999))).all().all()):
+            pass
+        else:
+            st.write('Something is wrong, most probably an invalid preference. '
+                     'Please chck your file, correct the mistake and upload the file again')
+            return
+
+        # Employee data:
+        # checking the lack of identical preference entries
+        test2 = (df2.iloc[:, 1:].apply(lambda row: row.dropna().nunique() == len(row), axis=1))
+        st.write(test2)
+        if all(test2):
+            pass
+        else:
+            row_index = [i for i in range(0, len(test2)) if test2[i] == False]
+            st.error(f'Error: Identical preference entries in position: '
+                     f'{df2_original.iloc[row_index[0]][0]}. Please correct the error.')
+            return
+
+        # checking that all data is ok (first condition = only numbers, second condition = valid preferences)
+        if ((df2.applymap(lambda x: isinstance(x, int)).all().all()) and
+                (df2.applymap(lambda y: (y < len(df1) + 1) or (y == 999))).all().all()):
+            pass
+        else:
+            st.write('Something is wrong, most probably an invalid preference. '
+                     'Please chck your file, correct the mistake and upload the file again')
+            return
+
+        st.write('##### Your file was uploaded successfully')
 
 
 
@@ -110,9 +154,15 @@ def extract_numbers_from_string(input_string, df):
             index_in_df = df[df.iloc[:, 0] == input_string].index.tolist()
             return index_in_df[0] + 1 if index_in_df else None
     else:
-        if len(str(input_string)) > 3:
+        if len(str(input_string)) > 4:
             index_in_df = df[df.iloc[:, 0] == input_string].index.tolist()
             return index_in_df[0] + 1 if index_in_df else None
         else:
             return input_string
+
+
+def check_integrity_of_data(df, df_original ):
+    # check that all entries are now integers
+    df.applymap(lambda x: isinstance(x, int)).all().all()
+
 
