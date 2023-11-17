1331dfd631c317ae77c4f90df35fedb383f56c87
dkrivaa <dannykrivaa@hotmail.com>
2023-11-17 13:47:45 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -65,11 +65,8 @@ def step2():
         df1 = pd.read_excel(file, sheet_name='Position Prefs').fillna(999)
         df2 = pd.read_excel(file, sheet_name='Candidate Prefs').fillna(999)
 
-        st.write(df1)
-
         df1_original = df1.copy()
         df2_original = df2.copy()
-
 
         # Making values into integers:
         # relevant position by index
@@ -79,59 +76,94 @@ def step2():
             df1[column] = df1[column].apply(extract_numbers_from_string, df=df2_original)
 
         # Making values into integers:
-        # relevant position by index
+        # relevant employee by index
         df2.iloc[:, 0] = df2.index + 1
         # preferences by extracting number from string (if string)
         for column in df2.columns[1:]:
             df2[column] = df2[column].apply(extract_numbers_from_string, df=df1_original)
 
-        st.write(df1)
-
         # Checking integrity of data in file
         # Position data:
         # checking the lack of identical preference entries
         test1 = (df1.iloc[:, 1:].apply(lambda row: row.dropna().nunique() == len(row), axis=1))
-        st.write(test1)
         if all(test1):
             pass
         else:
-            row_index = [i for i in range(0, len(test1)) if test1[i] == False]
-            st.error(f'Error: Identical preference entries in position: '
-                     f'{df1_original.iloc[row_index[0]][0]}. Please correct the error.')
-            return
+            row_index = [i for i in range(0, len(test1)) if test1[i] is False]
+            st.error(f'Error: Identical preference entries for position: '
+                     f'{df1_original.iloc[row_index[0]][0]}. Please correct the error '
+                     f'and upload file again.')
+            exit()
 
         # checking that all data is ok (first condition = only numbers, second condition = valid preferences)
         if ((df1.applymap(lambda x: isinstance(x, int)).all().all()) and
                 (df1.applymap(lambda y: (y < len(df2) + 1) or (y == 999))).all().all()):
             pass
         else:
-            st.write('Something is wrong, most probably an invalid preference. '
+            st.write('Something is wrong, most probably an invalid preference for one of the positions. '
                      'Please chck your file, correct the mistake and upload the file again')
-            return
+            exit()
 
         # Employee data:
         # checking the lack of identical preference entries
         test2 = (df2.iloc[:, 1:].apply(lambda row: row.dropna().nunique() == len(row), axis=1))
-        st.write(test2)
         if all(test2):
             pass
         else:
-            row_index = [i for i in range(0, len(test2)) if test2[i] == False]
-            st.error(f'Error: Identical preference entries in position: '
-                     f'{df2_original.iloc[row_index[0]][0]}. Please correct the error.')
-            return
+            row_index = [i for i in range(0, len(test2)) if test2[i] is False]
+            st.error(f'Error: Identical preference entries for employee: '
+                     f'{df2_original.iloc[row_index[0]][0]}. Please correct the error '
+                     f'and upload file again.')
+            exit()
 
         # checking that all data is ok (first condition = only numbers, second condition = valid preferences)
         if ((df2.applymap(lambda x: isinstance(x, int)).all().all()) and
-                (df2.applymap(lambda y: (y < len(df1) + 1) or (y == 999))).all().all()):
+                (df2.applymap(lambda y: (y < len(df2) + 1) or (y == 999))).all().all()):
             pass
         else:
-            st.write('Something is wrong, most probably an invalid preference. '
-                     'Please chck your file, correct the mistake and upload the file again')
-            return
+            st.write('Something is wrong, most probably an invalid preference for one of the employees. '
+                     'Please check your file, correct the mistake and upload the file again')
+            exit()
 
         st.write('##### Your file was uploaded successfully')
 
+        st.markdown('___')
+
+        # ______________________________________________________________________
+        # Step 3
+        st.write('### Step 3 - Press to MatchIT')
+
+        # Making data ready for MATCHING process
+        position_list = df1.iloc[:, 0].tolist()
+        position_pref_list = list(zip(*df1.iloc[:, 1:].values.T))
+        position_dict = dict(zip(position_list, position_pref_list))
+
+        employee_list = df2.iloc[:, 0].tolist()
+        employee_pref_list = list(zip(*df2.iloc[:, 1:].values.T))
+        employee_dict = dict(zip(employee_list, employee_pref_list))
+
+        # Getting the number of max preferences for upperhand (positions) and lowerhand (employees)
+        num_of_prefs_upperhand = len(max(position_pref_list, key=len))
+        num_of_prefs_lowerhand = len(max(employee_pref_list, key=len))
+
+        if num_of_prefs_upperhand not in st.session_state:
+            st.session_state.num_of_prefs_upperhand = num_of_prefs_upperhand
+
+        if num_of_prefs_lowerhand not in st.session_state:
+            st.session_state.num_of_prefs_lowerhand = num_of_prefs_lowerhand
+
+        # Calculating how many employees have matching preferences with positions
+        possible = 0
+        for employee in employee_list:
+            for position in employee_dict[employee]:
+                try:
+                    if employee in position_dict[position]:
+                        possible += 1
+                        break
+                except KeyError:
+                    pass
+
+        st.write(possible)
 
 
 # Help function for step 1
