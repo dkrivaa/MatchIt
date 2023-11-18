38fba784a11cd8aba2b3fc78dda0344a9d0b1004
dkrivaa <dannykrivaa@hotmail.com>
2023-11-18 11:09:40 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -36,10 +36,10 @@ def step1():
     with io.BytesIO() as buffer:
         # Use ExcelWriter to write multiple sheets to the buffer
         with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            df1.to_excel(writer, sheet_name='Position Prefs', startrow=1, startcol=0, index=False)
-            df2.to_excel(writer, sheet_name='Candidate Prefs', startrow=1, startcol=0, index=False)
+            df1.to_excel(writer, sheet_name='Position Prefs', startrow=2, startcol=1, index=False)
+            df2.to_excel(writer, sheet_name='Candidate Prefs', startrow=2, startcol=1, index=False)
 
-            # Defining the layout of excel file
+            # Defining the layout of Excel file
 
             # Getting the column letter of last column in excel file (two sheets)
             u_letter = chr(ord('A') + upperhand)
@@ -53,7 +53,7 @@ def step1():
             worksheet2.set_column(f'A:{l_letter}', 15)
 
             # Setting height of first line
-            worksheet1.set_row(0, 30)
+            worksheet1.set_row(1, 30)
 
         buffer.seek(0)
 
