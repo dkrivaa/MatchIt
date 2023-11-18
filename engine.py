2cb048b7d5a078ac90dbdc1705d028e8533f0045
dkrivaa <dannykrivaa@hotmail.com>
2023-11-18 20:50:11 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -166,8 +166,8 @@ def step2():
             exit()
 
         # checking that all data is ok (first condition = only numbers, second condition = valid preferences)
-        if ((df2.applymap(lambda x: isinstance(x, int) or isinstance(x, float)).all().all()) and
-                (df2.applymap(lambda y: (y < len(df1) + 1) or (y == 999)).all().all())):
+        if ((df2.iloc[:, 1:].applymap(lambda x: isinstance(x, int) or isinstance(x, float)).all().all()) and
+                (df2.iloc[:, 1:].applymap(lambda y: (y < len(df1) + 1) or (y == 999)).all().all())):
             pass
         else:
             st.write('Something is wrong, most probably an invalid preference for one of the candidates. '
