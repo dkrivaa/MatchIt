4fc91f9360382a858d07e34c32b68cfa8d96f540
dkrivaa <dannykrivaa@hotmail.com>
2023-11-18 21:35:48 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -15,7 +15,7 @@ def step1():
     if lang == 'English':
         st.write('### Step 1 - Make Excel file for download')
     else:
-        st.write('### שלב א - הכנת קובץ אקסל להורדה')
+        st.write('### שלב 1 - הכנת קובץ אקסל להורדה')
     col1, col2 = st.columns(2)
     with col1:
         upperhand = st.number_input('Max preferences Manager:', min_value=1)
