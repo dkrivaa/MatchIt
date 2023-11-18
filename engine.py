a2b3cb286b072df213b0776af5448947918167a2
dkrivaa <dannykrivaa@hotmail.com>
2023-11-18 18:52:47 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -136,8 +136,9 @@ def step2():
 
 
         st.write(df1)
-        st.write(df1.applymap(lambda x: isinstance(x, int)).all().all())
-
+        invalid_values_condition = df1.applymap(lambda x: not (isinstance(x, int) or x == 999))
+        invalid_values = df1[invalid_values_condition]
+        st.write(invalid_values)
 
         # checking that all data is ok (first condition = only numbers, second condition = valid preferences)
         if ((df1.applymap(lambda x: isinstance(x, int)).all().all()) and
