1a0841460e827f19a4adf99191234a0738f872fa
dkrivaa <dannykrivaa@hotmail.com>
2023-11-18 16:45:21 +0200

    MatchIt

--- engine.py
+++ engine.py
@@ -148,7 +148,7 @@ def step2():
         # Employee data:
         # checking the lack of identical preference entries
         test2 = (df2.iloc[:, 1:].apply(lambda row: row.dropna().nunique() == len(row), axis=1))
-        st.write(type(test2))
+        st.write(test2)
         if all(test2):
             pass
         else:
